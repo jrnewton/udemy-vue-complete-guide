81065f2a8163f9740d1292e9a1be57ad40db594a
Jon Newton <newt@hey.com>
2020-11-08 21:24:20 -0500

    Method example again

--- public/app.js
+++ public/app.js
@@ -6,6 +6,16 @@ const app = Vue.createApp({
       counter: 0
     };
   },
+  
+  methods: { 
+    add() { 
+      this.counter++;
+    }, 
+
+    reduce() { 
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
