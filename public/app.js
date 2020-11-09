5d0c9490f3e7d800820fa17c6bfd85208853f92a
Jon Newton <newt@hey.com>
2020-11-08 21:20:34 -0500

    Switch over to new events example app

--- public/app.js
+++ public/app.js
@@ -3,23 +3,9 @@
 const app = Vue.createApp({
   data() { 
     return {
-      goal1: 'Finish the course and learn Vue!',
-      goal2: 'Master Vue and building amazing apps!',
-      linkHref: 'https://vuejs.org',
-      linkName: 'about Vue'
+      counter: 0
     };
   },
-  methods: { 
-    outputGoal() { 
-      const rand = Math.random();
-      if (rand < 0.5) { 
-        return this.goal1;
-      }
-      else { 
-        return this.goal2;
-      }
-    }
-  }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
