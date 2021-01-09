b08b9e780b3ade00867525005e40c92e9a7570d1
Jon Newton <newt@hey.com>
2021-01-09 15:10:10 -0500

    special attack done

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
     };
   },
   computed: {
@@ -17,16 +18,27 @@ const app = Vue.createApp({
 
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
+    },
+
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     }
   },
   methods: {
     attackMonster() {
       this.monsterHealth -= getRandomValue(5, 12);
       this.attackPlayer();
+      this.currentRound++;
     },
 
     attackPlayer() {
       this.playerHealth -= getRandomValue(8, 15);
+    },
+
+    specialAttackMonster() {
+      this.monsterHealth -= getRandomValue(10, 25);
+      this.attackPlayer();
+      this.currentRound++;
     }
   }
 });
