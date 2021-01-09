57b9f67ac8f58ea70faaf8b50693f57ec34299d7
Jon Newton <newt@hey.com>
2021-01-09 17:39:36 -0500

    Add skeleton for main App

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Manual',
+          name: 'Manual Lorenz',
+          phone: '01234 56544 449',
+          email: 'manuel@localhost.com'
+        },
+        {
+          id: 'Julie',
+          name: 'Julie Jones',
+          phone: '01234 56544 449',
+          email: 'manuel@localhost.com'
+        }
+      ]
+    };
+  }
+};
+</script>
