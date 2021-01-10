8698de04dea3dad0121cd8739c970d149c1bc54c
Jon Newton <newt@hey.com>
2021-01-10 12:33:23 -0500

    Improve styling to header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header><h1>My Friends</h1></header>
     <ul>
       <friend-contact></friend-contact>
       <friend-contact></friend-contact>
