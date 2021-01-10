6c641f9da90af2f15935f3ffe356620bd6917d2b
Jon Newton <newt@hey.com>
2021-01-10 14:54:32 -0500

    Setup for lecture 99

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ContactDetail from './components/ContactDetail.vue';
-import NewContact from './components/NewContact.vue';
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
 
 const app = createApp(App);
-app.component('contact-detail', ContactDetail);
-app.component('new-contact', NewContact);
+
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
+
 app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'My Contacts';
+      args[0].title = 'Knowledge App';
       return args;
     });
   }
