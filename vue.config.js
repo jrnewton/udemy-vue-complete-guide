4a8135b9d61379d7ba65ef5639401f771fe2e530
Jon Newton <newt@hey.com>
2021-01-16 16:02:27 -0500

    Update for section 9

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].title = 'Knowledge App';
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Dummy App With No Real Purpose';
       return args;
     });
   }
