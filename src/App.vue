0b62db93b8379fc23b6378643aca8a65357b4c9e
Jon Newton <newt@hey.com>
2021-01-16 16:13:58 -0500

    Define local component and provide notes on syntax of components property

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <the-header></the-header>
+    <TheHeader />
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +11,24 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 export default {
+  //these components are local to this vue object
+  components: {
+    //syntax options
+    //1) 'the-header': TheHeader
+    //or
+    //2) TheHeader: TheHeader
+    //3) TheHeader
+    //
+    //#1 defines html element <the-header> which requires closing tag.
+    //#2 defines same html element as #1 and defines html element <TheHeader>.
+    //   <TheHeader> is a self closing tag: <TheHeader/>
+    //#3 uses modern JS syntax to expand TheHeader -> TheHeader: TheHeader
+
+    //'the-header': TheHeader
+    TheHeader
+  },
   data() {
     return {
       activeUser: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
@@ -9,8 +8,8 @@ import UserInfo from './components/UserInfo.vue';
 const app = createApp(App);
 
 //these are global components avaible for use anywhere in
-//the App and in any of it's children
-app.component('the-header', TheHeader);
+//the App and in any of it's children.
+//Vue needs to load all global components at once.
 app.component('base-badge', BaseBadge);
 app.component('badge-list', BadgeList);
 app.component('user-info', UserInfo);
