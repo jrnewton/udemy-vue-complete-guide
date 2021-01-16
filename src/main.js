9ba86356c870c29d343c9d418fd47a1c8d8937b2
Jon Newton <newt@hey.com>
2021-01-16 16:31:42 -0500

    Example of slots

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import BaseCard from './components/BaseCard.vue';
 import BaseBadge from './components/BaseBadge.vue';
 
 const app = createApp(App);
@@ -9,5 +10,6 @@ const app = createApp(App);
 //the App and in any of it's children.
 //Vue needs to load all global components at once.
 app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
