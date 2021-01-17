d4a4b2353aa4ec73203424e6633122b616981253
Jon Newton <newt@hey.com>
2021-01-17 14:03:57 -0500

    Lecture 124: display resources in <ul>

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="r of storedResources" :key="r.id">
+      <a href="{{ r.url }}" :title="r.description">{{ r.title }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: Math.random() + '',
+          title: 'Official Guide',
+          description: 'The official VueJs documentation',
+          url: 'https://vuejs.org'
+        },
+        {
+          id: Math.random() + '',
+          title: 'Google',
+          description: 'Learn to google...',
+          url: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
