f2ad4ef0ecc446add5395a3c9681093c4a7e063b
Jon Newton <newt@hey.com>
2021-01-17 15:14:51 -0500

    Add BaseButton which is pure UI only

--- src/components/resources/ResourceItem.vue
+++ src/components/resources/ResourceItem.vue
@@ -4,7 +4,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
