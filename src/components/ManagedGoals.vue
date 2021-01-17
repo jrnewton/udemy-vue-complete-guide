7b0accf05d5b8ab0078a631beb2eaa0953c924d0
Jon Newton <newt@hey.com>
2021-01-17 12:00:43 -0500

    Vue3 fragments

--- src/components/ManagedGoals.vue
+++ src/components/ManagedGoals.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
-    <h2>Managed Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="set">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid</h2>
-      <p>{{ errorMessage }}</p>
-      <button @click="confirmError">Ok</button>
-    </error-alert>
-  </div>
+  <!-- In vue2 a top level element was required in each template. 
+  In this example that would be the <div>. 
+    In vue3 you can have 'fragments' or multiple top level 
+    elements and we can get rid of the <div> (unless needed for 
+      styling or sematic reasons)
+-->
+  <!-- <div> -->
+  <h2>Managed Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="set">Set Goal</button>
+  <error-alert v-if="inputIsInvalid">
+    <h2>Input is invalid</h2>
+    <p>{{ errorMessage }}</p>
+    <button @click="confirmError">Ok</button>
+  </error-alert>
+  <!--  </div> -->
 </template>
 
 <script>
