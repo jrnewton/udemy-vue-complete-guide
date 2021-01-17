6cacff78295a83e5e4d6104bf55e5695de9675cc
Jon Newton <newt@hey.com>
2021-01-17 11:54:40 -0500

    Example of teleport

--- src/components/ManagedGoals.vue
+++ src/components/ManagedGoals.vue
@@ -3,11 +3,28 @@
     <h2>Managed Goals</h2>
     <input type="text" ref="goal" />
     <button @click="set">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid</h2>
-      <p>{{ errorMessage }}</p>
-      <button @click="confirmError">Ok</button>
-    </error-alert>
+
+    <!-- Rendering the error <dialog> this deep in the app's
+      DOM tree is not the best choice.  Can introduce styling 
+      bugs and accessiblity problems.  It would be 
+      better to render towards the root of the DOM tree. 
+    -->
+
+    <!-- Vue provides the 'teleport' tag which allows you to define 
+      the render location.  
+      
+      The value of 'to' attribute is a CSS selector
+        e.g. 
+          body (body element)
+          #id  (an element id)
+     -->
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid</h2>
+        <p>{{ errorMessage }}</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
