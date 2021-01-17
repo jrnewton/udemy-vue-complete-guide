9f20685dd1925428c60c0bdc4b257147976954e6
Jon Newton <newt@hey.com>
2021-01-17 14:59:20 -0500

    Add card component

--- src/components/resources/ResourceItem.vue
+++ src/components/resources/ResourceItem.vue
@@ -1,16 +1,16 @@
 <template>
   <!-- <a href="{{ r.url }}" :title="r.description">{{ r.title }}</a> -->
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a href="{{ url }}">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a href="{{ url }}">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
