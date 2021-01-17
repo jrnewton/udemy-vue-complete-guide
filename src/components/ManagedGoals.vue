6d85bc0a552f57b4f49a52bd1e29168b5f5ae4ca
Jon Newton <newt@hey.com>
2021-01-17 11:46:03 -0500

    Setup for Lecture 117 teleporting elements.
    Define an error dialog component and provide ability to close.
    
    Nothing new learned yet, although don't forgot about 'ref' attribute.
    
    https://v3.vuejs.org/guide/component-template-refs.html

--- src/components/ManagedGoals.vue
+++ src/components/ManagedGoals.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
     <h2>Managed Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="set">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>{{ errorMessage }}</p>
+      <button @click="confirmError">Ok</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+      errorMessage: null
+    };
+  },
+  methods: {
+    set() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+        this.errorMessage = 'Input is blank';
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+};
+</script>
