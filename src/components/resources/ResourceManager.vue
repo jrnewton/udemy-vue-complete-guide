32ca644db293f3d7a4ab9e35c615f38f4420cad0
Jon Newton <newt@hey.com>
2021-01-18 14:46:33 -0500

    Add keep-alive arround the dynamic component

--- src/components/resources/ResourceManager.vue
+++ src/components/resources/ResourceManager.vue
@@ -7,7 +7,9 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
