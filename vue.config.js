d62bbdf813c6a88309d61a1741777a19d6997a19
Jon Newton <newt@hey.com>
2021-01-23 12:58:22 -0500

    Update title

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'Survey';
+      args[0].title = 'Learning Survey';
       return args;
     });
   }
