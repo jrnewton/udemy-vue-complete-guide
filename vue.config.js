e154a3275b56e22c0b087f53f4cbf50283ce7257
Jon Newton <newt@hey.com>
2021-01-23 12:48:06 -0500

    Change app title

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].title = 'Remember Me!';
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Survey';
       return args;
     });
   }
