25cc662d3f087ab48c015ddfbfc6b119a15c8031
Jon Newton <newt@hey.com>
2021-02-12 16:19:24 -0500

    Organizing your route code.
    Put components targetted by routes into pages dir.
    
    Pull route definition and config into routes.js and import into main.js

---
+++ src/pages/readme.md
@@ -0,0 +1,1 @@
+This directory contains Vue components that loaded through the router (as defined by rules in main.js).
---
+++ src/router.js
@@ -0,0 +1,126 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './pages/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersList from './pages/UsersList.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+import NotFound from './pages/NotFound.vue';
+import Auth from './pages/Auth.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    //order matters! first wins
+    {
+      name: 'root',
+      path: '/',
+      //see also alias
+      redirect: '/teams'
+    },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter
+      },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          //pass dynamic path elements to component via props
+          props: true,
+          meta: {
+            needsAuth: true
+          }
+        }
+      ]
+    },
+    {
+      name: 'users',
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter
+      },
+      //2. Route guard: route specific - called second
+      beforeEnter(to, from, next) {
+        //console.log('Route guard / route / beforeEnter');
+        next();
+      }
+    },
+    {
+      name: 'auth',
+      path: '/auth',
+      component: Auth
+    },
+    {
+      name: 'not-found',
+      //pass regex to variable
+      path: '/:notFound(.*)',
+      // this will go to default unamed router view in App.vue
+      component: NotFound
+    }
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    //to and from are router objects.
+    //same as this.$route inside a component
+    // console.log('to', _to);
+    // console.log('from', _from);
+
+    //position when user navigated forward
+    //console.log('pos', savedPosition);
+
+    //savedPosition will be null when navigating forward
+    //and it will have some value when navigating backward.
+    //Use that to properly set the scroll view.
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return {
+        left: 0,
+        top: 0
+      };
+    }
+  }
+});
+
+//1. Route guard: Global - called first.
+router.beforeEach((to, from, next) => {
+  console.log('Route guard / global / beforeEach');
+
+  if (to.meta.needsAuth) {
+    next({
+      name: 'auth',
+      query: {
+        fromRoute: from.name
+      }
+    });
+  } else {
+    next();
+  }
+
+  // next(); //continue as-is
+  // next(false); //cancel navigation
+  // next(true); //continue as-is
+  // next('/error'); //redirect to URL
+
+  //silly example to always direct to team 2
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   //redirect to route object
+  //   next({
+  //     name: 'team-members',
+  //     params: { teamId: 't2' }
+  //   });
+  // }
+});
+
+router.afterEach((to, from) => {
+  console.log('Global afterEach', to.name, from.name);
+});
+
+export default router;
