e2385380e4f2ebd814fd96d94c3a6cd0a0ca08fa
Jon Newton <newt@hey.com>
2021-02-12 16:01:07 -0500

    beforeRouteLeave with custom dialog

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,6 @@
 <template>
+  <button @click="$router.push('/teams')">Confirm</button>
+  <button @click="changesSaved = true">Save</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -7,12 +9,24 @@
       :role="user.role"
     ></user-item>
   </ul>
+  <teleport to="#error">
+    <dialog open v-if="displayWarning">
+      <h3>Please don't leave!</h3>
+      Changes not saved
+    </dialog>
+  </teleport>
 </template>
 
 <script>
 import UserItem from './UserItem.vue';
 
 export default {
+  data() {
+    return {
+      changesSaved: false,
+      displayWarning: false
+    };
+  },
   components: {
     UserItem
   },
@@ -21,6 +35,24 @@ export default {
   beforeRouteEnter(to, from, next) {
     //('Route guard / component / beforeRouteEnter');
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('beforeRouteLeave');
+    if (this.changesSaved) {
+      next();
+    } else {
+      //cancel navigation
+      next(false);
+      //display warning to user
+      this.displayWarning = true;
+      //close warning after 5s
+      setTimeout(() => {
+        this.displayWarning = false;
+      }, 5000);
+    }
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 };
 </script>
