5dafc08b663ecfbf025eaaa3422e96ffd36bf3d0
Jon Newton <newt@hey.com>
2021-04-02 21:18:52 -0400

    vue.config: fix page title

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'Team Members';
+      args[0].title = 'Vuex';
       return args;
     });
   }
